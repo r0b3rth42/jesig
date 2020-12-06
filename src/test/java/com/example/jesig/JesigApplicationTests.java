71c89c040f4e750447de337653fc9bc9d5ca956d
Roberth Angel Irazaba Garcia <irazabarob44@gmail.com>
2020-12-05 21:19:33 -0500

    primer comit

---
+++ src/test/java/com/example/jesig/JesigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jesig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JesigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
